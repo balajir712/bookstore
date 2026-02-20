e8929371b3a0904afceb61200da7570452809a84
balajir712@ <balajir712@gmail.com>
2026-02-20 13:28:01 +0530

    Workflow changes 2

--- catalog-service/src/main/java/com/codenova/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/codenova/catalog_service/CatalogServiceApplication.java
@@ -11,4 +11,5 @@ public class CatalogServiceApplication {
 	}
 
 	//This is the main class for the Catalog Service application. It uses the @SpringBootApplication annotation to indicate that it is a Spring Boot application. The main method starts the application by calling SpringApplication.run() with the CatalogServiceApplication class and the command-line arguments.
+	// The Catalog Service is responsible for managing the catalog of products in the e-commerce application. It provides APIs for retrieving product information, adding new products, updating existing products, and deleting products from the catalog. The service interacts with a database to store and retrieve product data.
 }
